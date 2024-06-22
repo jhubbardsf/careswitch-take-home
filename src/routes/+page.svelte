087c89ac9f4f792b6118bc7120df3ce34d5b60e0
Josh Hubbard <jhubbardsf@gmail.com>
2024-06-22 14:01:50 -0400

    Commit before seed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,75 @@
 <script lang="ts">
-	import * as Card from '$lib/components/ui/card';
+	import CircleUser from 'lucide-svelte/icons/circle-user';
+	import Menu from 'lucide-svelte/icons/menu';
+	import Package2 from 'lucide-svelte/icons/package-2';
+	import Search from 'lucide-svelte/icons/search';
+
+	import { Button } from '$lib/components/ui/button/index.js';
+	import * as Card from '$lib/components/ui/card/index.js';
+	import { Checkbox } from '$lib/components/ui/checkbox/index.js';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import * as Sheet from '$lib/components/ui/sheet/index.js';
+	import { cn } from '$lib/utils';
+	import { page } from '$app/stores';
 </script>
 
-<div>
-	<Card.Root class="w-[350px]">
-		<Card.Header>
-			<Card.Title class="flex items-center justify-center">CareSwitch Example</Card.Title>
-			<Card.Description class="pt-4"
-				>Josh Hubbard creating something that CareSwitch will be pleased with.</Card.Description
-			>
-		</Card.Header>
-		<Card.Content></Card.Content>
-		<Card.Footer class="flex items-center justify-center">Svelte v5</Card.Footer>
-	</Card.Root>
+<div class="mx-auto grid w-full max-w-6xl gap-2">
+	<h1 class="text-3xl font-semibold">Settings</h1>
+</div>
+<div
+	class="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]"
+>
+	<nav
+		class="grid gap-4 text-sm text-muted-foreground"
+		data-x-chunk-container="chunk-container after:right-0"
+	>
+		<a href="##" class="font-semibold text-primary"> General </a>
+		<a href="##">Security</a>
+		<a href="##">Integrations</a>
+		<a href="##">Support</a>
+		<a href="##">Organizations</a>
+		<a href="##">Advanced</a>
+	</nav>
+	<div class="grid gap-6">
+		<Card.Root>
+			<Card.Header>
+				<Card.Title>Store Name</Card.Title>
+				<Card.Description>Used to identify your store in the marketplace.</Card.Description>
+			</Card.Header>
+			<Card.Content>
+				<form>
+					<Input placeholder="Store Name" />
+				</form>
+			</Card.Content>
+			<Card.Footer class="border-t px-6 py-4">
+				<Button>Save</Button>
+			</Card.Footer>
+		</Card.Root>
+		<Card.Root>
+			<Card.Header>
+				<Card.Title>Plugins Directory</Card.Title>
+				<Card.Description>
+					The directory within your project, in which your plugins are located.
+				</Card.Description>
+			</Card.Header>
+			<Card.Content>
+				<form class="flex flex-col gap-4">
+					<Input placeholder="Project Name" value="/content/plugins" />
+					<div class="flex items-center space-x-2">
+						<Checkbox id="include" checked={true} />
+						<label
+							for="include"
+							class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+						>
+							Allow administrators to change the directory.
+						</label>
+					</div>
+				</form>
+			</Card.Content>
+			<Card.Footer class="border-t px-6 py-4">
+				<Button>Save</Button>
+			</Card.Footer>
+		</Card.Root>
+	</div>
 </div>
--- src/scripts/seed.js
+++ src/scripts/seed.js
@@ -3,26 +3,31 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function seed() {
-	const workspaces = await prisma.$transaction(async (txn) => {
-		await txn.workspace.deleteMany();
-		return await txn.workspace.createManyAndReturn({
+	await prisma.$transaction(async (txn) => {
+		const workspaces = await txn.workspace.createMany({
 			data: [{ name: 'Workspace 1' }, { name: 'Workspace 2' }]
 		});
-	});
 
-	const users = await prisma.$transaction(async (txn) => {
-		await txn.user.deleteMany();
-		return await txn.user.createManyAndReturn({
+		const users = await txn.user.createMany({
 			data: [
 				{ name: 'Alice', email: 'alice@careswitch.com' },
 				{ name: 'Bob', email: 'bob@careswitch.com' }
 			]
 		});
-	});
 
-	console.log(`Created users: ${JSON.stringify(users)}`);
+		const alice = await txn.user.findUnique({ where: { email: 'alice@careswitch.com' } });
+		const bob = await txn.user.findUnique({ where: { email: 'bob@careswitch.com' } });
+
+		console.log(`Created users: ${JSON.stringify(users)}`);
+		console.log(`Created workspaces: ${JSON.stringify(workspaces)}`);
+	});
 }
 
-seed().finally(async () => {
-	await prisma.$disconnect();
-});
+seed()
+	.catch((e) => {
+		console.error(e);
+		process.exit(1);
+	})
+	.finally(async () => {
+		await prisma.$disconnect();
+	});
